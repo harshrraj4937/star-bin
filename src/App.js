4bf4b799bb3b366b3b04167e4b9021f392c0d0b2
harshraj <harshraj@stackfusion.io>
2024-10-12 21:07:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <Footer />
     </div>
   );
 }
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="contact">
+        <p>Why choose us?</p>
+        {/* Add more content here */}
+      </div>
+    </footer>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo" alt="Boongg Logo" />
+      </div>
+      <nav className="nav">
+        <a href="#prices">Prices</a>
+        <a href="#partner">Become a Partner</a>
+        <a href="#monthly-rental">Monthly Bike Rental</a>
+        <a href="#login">Login</a>
+      </nav>
+    </header>
+  );
+};
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>
+        Rent Bike & Scooty Now In <span>Your City</span>
+      </h1>
+      <p>10k+ | 4.6 Rating</p>
+      <div className="rental-info">
+        <div className="rental-card">
+          <p>Hourly Bike Rental</p>
+          <p>Starts @ ₹39*</p>
+        </div>
+        <div className="rental-card">
+          <p>Daily Bike Rental</p>
+          <p>Starts @ ₹349*</p>
+        </div>
+        <div className="rental-card">
+          <p>Monthly Bike Rental</p>
+          <p>Starts @ ₹3999*</p>
+        </div>
+      </div>
+
+      <div className="booking-form">
+        <div className="form-group">
+          <label>Current City</label>
+          <input type="text" placeholder="Select City" />
+        </div>
+        <div className="form-group">
+          <label>Start Date and Time</label>
+          <input type="datetime-local" />
+        </div>
+        <div className="form-group">
+          <label>End Date and Time</label>
+          <input type="datetime-local" />
+        </div>
+        <button className="rent-now-btn">Rent Now</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
